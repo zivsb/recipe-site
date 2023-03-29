a2f86bd6755e23aa37b7e24b49cf555682cbf843
Ziv Weissman <ziv@ucsb.edu>
2023-03-28 22:22:40 -0700

    Implemented basic functionallity to all features

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const app = express();
 
-const helmet = require('helmet');
-app.use(helmet());
+// const helmet = require('helmet');
+// app.use(helmet());
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -13,15 +14,49 @@ app.use(express.urlencoded());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Retrieving the posts from the json file
+const posts = JSON.parse(fs.readFileSync('posts.json'));
+console.log(posts);
+
+function storePosts() {
+    fs.writeFileSync('posts.json', JSON.stringify(posts));
+    return ("Successfully stored: ", posts);
+}
+
 app.get('/', (req, res, next)=> {
-    console.log(req.query);
-    res.render('home');
+    res.render('home', {
+        showPosts: posts.slice(1).slice(-5)
+    });
 })
 
-app.get('/extensions/:ext', (req, res, next) => {
-    console.log(req.params.ext);
-    res.locals.extension = req.params.ext;
-    res.render('extension')
+app.get('/add-recipe', (req, res, next) => {
+    res.render('addRecipe');
+})
+
+app.get('/recipe/:i', (req, res, next) => {
+    console.log(req.params.i);
+    res.render('recipe', posts[req.params.i]);
+})
+
+app.post('/add-recipe', (req, res, next) => {
+    console.log(req.body);
+    posts.push({
+        "i": posts.length,
+        "name": req.body.recipeName,
+        "ingredients": req.body.ingredients,
+        "instructions": req.body.instructions,
+        "creator": req.body.creator
+    });
+
+    storePosts();
+
+    res.redirect('/');
+})
+
+// A post request for me to run to save the data
+// Run this before shutting down server
+app.post('/save', (req, res, next)=> {
+    
 })
 
 app.listen(3000);
